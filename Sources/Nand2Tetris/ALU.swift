7a4ef78537dabdcf1509798a168a01eaad349776
Daniel Segall <segalld@gmail.com>
2021-12-06 19:55:55 +0000

    Complete addition

--- Sources/Nand2Tetris/ALU.swift
+++ Sources/Nand2Tetris/ALU.swift
@@ -1,6 +1,6 @@
 func halfAdder(_ a: Int, _ b: Int) -> IntX2 {
     IntX2(carry: and(a, b),
-          sum:   xor(a, b))
+          sum: xor(a, b))
 }
 
 func fullAdder(_ a: Int, _ b: Int, _ c: Int) -> IntX2 {
@@ -8,10 +8,22 @@ func fullAdder(_ a: Int, _ b: Int, _ c: Int) -> IntX2 {
     let addABC = halfAdder(addAB.sum, c)
     
     return IntX2(carry: or(addABC.carry, addAB.carry),
-                 sum:   addABC.sum)
+                 sum: addABC.sum)
 }
 
-extension IntX2 {
+func add16(_ a: IntX16, _ b: IntX16) -> IntX16 {
+    zip(a.reversed(), b.reversed()).reduce(into: (sum: [Int](), carry: 0)) { (result, addends) in
+        let addition = fullAdder(addends.0, addends.1, result.carry)
+        result.sum += [addition.sum]
+        result.carry = addition.carry
+    }.0.reversed().x16
+}
+
+func inc16(_ a: IntX16) -> IntX16 {
+    add16(a, [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1].x16)
+}
+
+private extension IntX2 {
     var sum: Int { self[1] }
     var carry: Int { self[0] }
     
