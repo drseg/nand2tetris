d793715104cdd5ae0f7291bc54cfa9f97e551757
Daniel Segall <segalld@gmail.com>
2021-12-06 16:02:47 +0000

    Clarification

--- Sources/Nand2Tetris/ALU.swift
+++ Sources/Nand2Tetris/ALU.swift
@@ -1,15 +1,21 @@
 func halfAdder(_ a: Int, _ b: Int) -> IntX2 {
-    [and(a, b), xor(a, b)].x2
+    IntX2(carry: and(a, b),
+          sum:   xor(a, b))
 }
 
 func fullAdder(_ a: Int, _ b: Int, _ c: Int) -> IntX2 {
     let addAB = halfAdder(a, b)
     let addABC = halfAdder(addAB.sum, c)
     
-    return [or(addABC.carry, addAB.carry), addABC.sum].x2
+    return IntX2(carry: or(addABC.carry, addAB.carry),
+                 sum:   addABC.sum)
 }
 
 extension IntX2 {
     var sum: Int { self[1] }
-    var carry: Int {self[0] }
+    var carry: Int { self[0] }
+    
+    convenience init(carry: Int, sum: Int) {
+        self.init([carry, sum])
+    }
 }
