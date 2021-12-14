92a12404739c73391e509dcfbd583cbfe3e5434a
Daniel Segall <segalld@gmail.com>
2021-12-14 18:38:33 +0000

    Refactor Adder

--- Sources/Nand2Tetris/Adder.swift
+++ Sources/Nand2Tetris/Adder.swift
@@ -1,35 +1,40 @@
-//func halfAdder(_ a: Int, _ b: Int) -> IntX2 {
-//    IntX2(carry: and(a, b),
-//          sum: xor(a, b))
-//}
-//
-//func fullAdder(_ a: Int, _ b: Int, _ c: Int) -> IntX2 {
-//    let addAB = halfAdder(a, b)
-//    let addABC = halfAdder(addAB.sum, c)
-//    
-//    return IntX2(carry: or(addABC.carry, addAB.carry),
-//                 sum: addABC.sum)
-//}
-//
-//func add16(_ a: IntX16, _ b: IntX16) -> IntX16 {
-//    zip(a,b).reversed().reduce(into: (sum: [Int](), carry: 0)) { (result, addends) in
-//        let addition = fullAdder(addends.0, addends.1, result.carry)
-//        result.sum += [addition.sum]
-//        result.carry = addition.carry
-//    }.0.reversed().x16
-//}
-//
-//func inc16(_ a: IntX16) -> IntX16 {
-//    let one = not16(add16(not16(zero(a)), not16(zero(a))))
-//    
-//    return add16(a, one)
-//}
-//
-//private extension IntX2 {
-//    var sum: Int { self[1] }
-//    var carry: Int { self[0] }
-//    
-//    convenience init(carry: Int, sum: Int) {
-//        self.init([carry, sum])
-//    }
-//}
+func halfAdder(_ a: Character, _ b: Character) -> String {
+    String(carry: and(a, b), sum: xor(a, b))
+}
+
+func fullAdder(_ a: Character, _ b: Character, _ c: Character) -> String {
+    let addAB = halfAdder(a, b)
+    let addABC = halfAdder(addAB.sum, c)
+    
+    return String(carry: or(addABC.carry, addAB.carry),
+                 sum: addABC.sum)
+}
+
+func add16(_ a: String, _ b: String) -> String {
+    String(
+        zip(a,b).reversed().reduce(into: (sum: "", carry: Character("0"))) { (result, addends) in
+            let addition = fullAdder(addends.0, addends.1, result.carry)
+            result.sum.append(addition.sum)
+            result.carry = addition.carry
+        }.0.reversed()
+    )
+}
+
+func inc16(_ a: String) -> String {
+    let one = not16(add16(not16(zero(a)), not16(zero(a))))
+    return add16(a, one)
+}
+
+func zero(_ a: String) -> String {
+    let out = xor(a[0], a[0])
+    return String(repeating: out, count: 16)
+}
+
+private extension String {
+    var sum: Character { self[1] }
+    var carry: Character { self[0] }
+    
+    init(carry: Character, sum: Character) {
+        self.init([carry, sum])
+    }
+}
