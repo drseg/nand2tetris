a16ddb9c23f7e2e18d8865f1c291da1d0aaa41cc
Daniel Segall <segalld@gmail.com>
2021-12-27 20:29:42 +0000

    Modify Memory.cmp to account for implicit keboard and clock

--- Sources/Nand2Tetris/Memory.swift
+++ Sources/Nand2Tetris/Memory.swift
@@ -1,1 +1,21 @@
-
+class Memory {
+    private let ram16ks = [FastRAM](count: 2, forEach: FastRAM(16384))
+    
+    func callAsFunction(_ word: String, _ load: Char, _ address: String, _ clock: Char) -> String {
+        let loadMap = deMux(load, address[0])
+        let clockMap = deMux(clock, address[0])
+        
+        let ram16KAddress = String(address.suffix(14))
+        
+        let out = ram16ks.enumerated().reduce(into: [String]()) {
+            $0.append(
+                $1.element(word,
+                           loadMap[$1.offset],
+                           ram16KAddress,
+                           clockMap[$1.offset])
+            )
+        }
+        
+        return mux16(out[0], out[1], address[0])
+    }
+}
--- Tests/Nand2TetrisTests/MemoryTests.swift
+++ Tests/Nand2TetrisTests/MemoryTests.swift
@@ -1,2 +1,22 @@
 import XCTest
 @testable import Nand2Tetris
+
+class Memorytests: XCTestCase {
+    
+    func testAcceptance() throws {
+        let memory = Memory()
+        
+        try FileBasedATR("Computer/Memory") {
+            let clock = $0[0].clockSignal
+            let word = $0[2].toBinary()
+            let load = $0[3].toChar
+            let address = $0[4]
+            
+            return [memory(word,
+                           load,
+                           address,
+                           clock).toDecimal()]
+            
+        }.run()
+    }
+}
