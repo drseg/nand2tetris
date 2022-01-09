657b629c3c95101328e1afe572d6fcb1532b60eb
Daniel Segall <segalld@gmail.com>
2022-01-09 17:27:11 +0000

    Parse pseudocommands

--- Sources/Nand2Tetris/Software/Assembler/SymbolResolver.swift
+++ Sources/Nand2Tetris/Software/Assembler/SymbolResolver.swift
@@ -1,5 +1,31 @@
 class SymbolResolver {
-    func resolve(_ assembly: String) -> [String: String] {
-        [:]
+    func resolve(_ assembly: String)  -> [String: Int] {
+        var lineNumber = 0
+        
+        return assembly
+            .components(separatedBy: "\n")
+            .reduce(into: [String: Int]()) { result, line in
+                lineNumber += 1
+                if let pseudoCommand = line
+                    .droppingComments
+                    .pseudoCommand {
+                    result[pseudoCommand] = lineNumber
+                }
+            }
+    }
+}
+
+extension String {
+    var droppingComments: String {
+        components(separatedBy: "//")[0]
+    }
+    
+    var pseudoCommand: String? {
+        guard let range = range(of: "[(][^0-9][^()]*[)]",
+                                options: .regularExpression) else {
+            return nil
+        }
+        
+        return String(self[range].dropFirst().dropLast())
     }
 }
--- Tests/Nand2TetrisTests/Software/Assembler/SymbolResolverTests.swift
+++ Tests/Nand2TetrisTests/Software/Assembler/SymbolResolverTests.swift
@@ -8,15 +8,42 @@ class SymbolResolverTests: XCTestCase {
         resolver = SymbolResolver()
     }
     
-    func resolve(_ assembly: String) -> [String: String] {
+    func resolve(_ assembly: String) -> [String: Int] {
         resolver.resolve(assembly)
     }
     
     func testResolvesEmpty() {
         resolve("") => [:]
+        resolve(" ") => [:]
+    }
+    
+    func testResolvesSinglePseudoCommand() {
+        resolve("LOOP") => [:]
+        resolve("(LOOP)") => ["LOOP": 1]
     }
     
     func testDoesNotResolveComments() {
         resolve("//(LOOP)") => [:]
+    }
+    
+    func testResolvesMultiplePseudoCommands() {
+        resolve("(LOOP)\n(END)") => ["LOOP": 1, "END": 2]
+    }
+    
+    func testIncrementsCommandAddressCorrectly() {
+        resolve("(LOOP)\nM=A\n(END)") => ["LOOP": 1, "END": 3]
+    }
+    
+    func testDoesNotResolveCommentOnSameLineAsPseudoCommand() {
+        resolve("(LOOP)//(NULL)\n(END)//(NULL)") => ["LOOP": 1, "END": 2]
+    }
+    
+    func testOnlyResolvesOneCommandPerLine() {
+        resolve("(LOOP)(LOOP)") => ["LOOP": 1]
+    }
+    
+    func testDisallowsLeadingDigit() {
+        resolve("(1LOOP)") => [:]
+        resolve("(LOOP1)") => ["LOOP1": 1]
     }
 }
