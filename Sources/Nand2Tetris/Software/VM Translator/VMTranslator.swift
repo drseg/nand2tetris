219a6701d85581100d95c8f81a498c44c89abf4f
Daniel Segall <segalld@gmail.com>
2022-01-14 12:00:15 +0000

    Push constant

--- Sources/Nand2Tetris/Software/VM Translator/VMTranslator.swift
+++ Sources/Nand2Tetris/Software/VM Translator/VMTranslator.swift
@@ -1,3 +1,21 @@
 class VMTranslator {
+    func translate(_ vmCode: String) -> String {
+        vmCode.lines.reduce(into: [String]()) { result, line in
+            let components = line.components(separatedBy: " ")
+            let value = components[2]
+            
+            result += [translatePushConstant(value)]
+        }.joined(separator: "\n")
+    }
     
+    func translatePushConstant(_ c: String) -> String {
+        """
+        @\(c)
+        D=A
+        @SP
+        A=M
+        M=D
+        M=M+1
+        """
+    }
 }
