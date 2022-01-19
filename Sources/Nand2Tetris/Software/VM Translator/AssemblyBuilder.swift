59f1c9d7a52b559d7f3241334c5100cbb519392d
Daniel Segall <segalld@gmail.com>
2022-01-19 13:34:10 +0000

    Refactor VMTranslator to use separate builder

---
+++ Sources/Nand2Tetris/Software/VM Translator/AssemblyBuilder.swift
@@ -0,0 +1,271 @@
+class AssemblyBuilder {
+    private (set) var assembly = ""
+    
+    func popSegmentWithMnemonic(
+        to mnemonic: String,
+        at offset: String
+    ) {
+        append(
+        """
+        \(setRegister("D", mnemonic: mnemonic, offset: offset))
+        @R15
+        M=D
+        \(popStack())
+        @R15
+        A=M
+        M=D
+        """
+        )
+    }
+    
+    func popSegmentWithValue(
+        to value: String,
+        at offset: String
+    ) {
+        append(
+        """
+        \(setRegister("D", value: value, offset: offset))
+        @R15
+        M=D
+        \(popStack())
+        @R15
+        A=M
+        M=D
+        """
+        )
+    }
+    
+    func pushSegmentWithMnemonic(
+        _ mnemonic: String,
+        at offset: String
+    ) {
+        append(
+        """
+        \(setRegister("A", mnemonic: mnemonic, offset: offset))
+        D=M
+        \(pushDToStack())
+        """
+        )
+    }
+    
+    func pushSegmentWithValue(
+        _ value: String,
+        at offset: String
+    ) {
+        append(
+        """
+        \(setRegister("A", value: value, offset: offset))
+        D=M
+        \(pushDToStack())
+        """
+        )
+    }
+    
+    func pushConstant(_ c: String) {
+        c[0] == "-"
+        ? pushNegativeConstant(c)
+        : pushPositiveConstant(c)
+    }
+    
+    func eq(_ count: String) {
+        bool("EQ" + count)
+    }
+    
+    func gt(_ count: String) {
+        bool("GT" + count)
+    }
+    
+    func lt(_ count: String) {
+        bool("LT" + count)
+    }
+    
+    func add() {
+        binaryOperation("+")
+    }
+    
+    func sub() {
+        binaryOperation("-")
+    }
+    
+    func and() {
+        binaryOperation("&")
+    }
+    
+    func or() {
+        binaryOperation("|")
+    }
+    
+    func not() {
+        unaryOperation("!")
+    }
+    
+    func neg() {
+        unaryOperation("-")
+    }
+    
+    func addLabel(_ label: String) {
+        append(
+        """
+        (\(label))
+        """
+        )
+    }
+    
+    func goto(_ label: String) {
+        append(
+        """
+        @\(label)
+        0;JMP
+        """
+        )
+    }
+    
+    func ifGoto(_ label: String) {
+        append(
+        """
+        \(popStack())
+        @\(label)
+        D;JEQ
+        """
+        )
+    }
+    
+    private func setRegister(
+        _ register: String,
+        mnemonic: String,
+        offset: String = "0"
+    ) -> String {
+        setWithOffsetAddend(register, mnemonic, offset, "M")
+    }
+    
+    private func setRegister(
+        _ register: String,
+        value: String,
+        offset: String = "0"
+    ) -> String {
+        setWithOffsetAddend(register, value, offset, "A")
+    }
+    
+    private func setWithOffsetAddend(
+        _ register: String,
+        _ value: String,
+        _ offset: String,
+        _ addend: String
+    ) -> String {
+        """
+        @\(offset)
+        D=A
+        @\(value)
+        \(register)=D+\(addend)
+        """
+    }
+    
+    private func pushPositiveConstant(_ c: String) {
+        append(
+        """
+        @\(c)
+        D=A
+        \(pushDToStack())
+        """
+        )
+    }
+    
+    private func pushNegativeConstant(_ c: String) {
+        append(
+        """
+        @\(c.dropFirst())
+        D=-A
+        \(pushDToStack())
+        """
+        )
+    }
+    
+    private func pushDToStack() -> String {
+        """
+        \(aEqualsSP())
+        M=D
+        \(incrementSP())
+        """
+    }
+    
+    private func bool(_ predicate: String) {
+        sub()
+        append(
+        """
+        @\(predicate + "_TRUE")
+        D;J\(predicate.prefix(2))
+        D=-1
+        \(replaceTop())
+        @\(predicate + "_FALSE")
+        0;JMP
+        (\(predicate + "_TRUE"))
+        D=0
+        \(replaceTop())
+        (\(predicate + "_FALSE"))
+        """
+        )
+    }
+    
+    private func binaryOperation(_ o: String) {
+        append(
+        """
+        \(popStack())
+        A=M-1
+        D=\(o == "-" ? "M-D" : "D\(o)M")
+        \(replaceTop())
+        """
+        )
+    }
+    
+    private func unaryOperation(_ sign: String) {
+        append(
+        """
+        \(aEqualsSP(offset: "-1"))
+        D=\(sign)M
+        M=D
+        \(replaceTop())
+        """
+        )
+    }
+    
+    private func aEqualsSP(offset: String = "") -> String {
+        """
+        @SP
+        A=M\(offset)
+        """
+    }
+    
+    private func popStack() -> String {
+        """
+        \(aEqualsSP(offset: "-1"))
+        D=M
+        \(decrementSP())
+        """
+    }
+    
+    private func replaceTop() -> String {
+        """
+        \(aEqualsSP(offset: "-1"))
+        M=D
+        """
+    }
+    
+    private func incrementSP() -> String {
+        stepSP(sign: "+")
+    }
+    
+    private func decrementSP() -> String {
+        stepSP(sign: "-")
+    }
+    
+    private func stepSP(sign: String) -> String {
+        """
+        @SP
+        M=M\(sign)1
+        """
+    }
+    
+    private func append(_ a: String) {
+        assembly += (assembly.isEmpty ? "" : "\n") + a
+    }
+}
--- Sources/Nand2Tetris/Software/VM Translator/VMTranslator.swift
+++ Sources/Nand2Tetris/Software/VM Translator/VMTranslator.swift
@@ -9,158 +9,122 @@ class VMTranslator {
         }
     }
     
+    private let builder: AssemblyBuilder
+    
+    init(_ builder: AssemblyBuilder = AssemblyBuilder()) {
+        self.builder = builder
+    }
+    
     func toAssembly(_ vm: String, file: String = #fileID) -> String {
         vm.lines
             .enumerated()
-            .map { toAssembly(VMLine(code: $0.element,
+            .forEach { toAssembly(VMLine(code: $0.element,
                                      fileName: file,
                                      index: $0.offset)) }
-            .joined(separator: "\n")
+        return builder.assembly
     }
     
-    func toAssembly(_ line: VMLine) -> String {
+    private func toAssembly(_ line: VMLine) {
         switch line.words.count {
-        case 1: return computationToAssembly(line)
-        case 2: return branchingToAssebly(line)
-        case 3: return memoryAccessToAssembly(line)
+        case 1: computationToAssembly(line)
+        case 2: branchingToAssebly(line)
+        case 3: memoryAccessToAssembly(line)
         default:
             fatalError("Lines can't have \(line.words.count) words")
         }
     }
     
-    func computationToAssembly(_ line: VMLine) -> String {
+    private func computationToAssembly(_ line: VMLine) {
         switch line.code {
-        case "add": return add()
-        case "sub": return sub()
-        case "not": return not()
-        case "neg": return neg()
-        case "and": return and()
-        case "or": return or()
-        case "eq": return eq(String(line.index))
-        case "gt": return gt(String(line.index))
-        case "lt": return lt(String(line.index))
+        case "add": builder.add()
+        case "sub": builder.sub()
+        case "not": builder.not()
+        case "neg": builder.neg()
+        case "and": builder.and()
+        case "or": builder.or()
+        case "eq": builder.eq(String(line.index))
+        case "gt": builder.gt(String(line.index))
+        case "lt": builder.lt(String(line.index))
         default:
             fatalError("Unrecognised computation '\(line.code)'")
         }
     }
     
-    func branchingToAssebly(_ line: VMLine) -> String {
+    private func branchingToAssebly(_ line: VMLine) {
         let words = line.words
         
         let command = words[0]
         let label = words[1]
         
         switch command {
-        case "label": return addLabel(label)
-        case "goto": return goto(label)
-        case "if-goto": return ifGoto(label)
+        case "label": builder.addLabel(label)
+        case "goto": builder.goto(label)
+        case "if-goto": builder.ifGoto(label)
         default:
             fatalError("Unrecognised branching command '\(command)'")
         }
     }
     
-    func memoryAccessToAssembly(_ line: VMLine) -> String {
+    private func memoryAccessToAssembly(_ line: VMLine) {
         let words = line.words
         
         let command = words[0]
         let segment = words[1]
         let offset = words[2]
         
-        switch command {
-        case "push" where segment == "constant":
-            return pushConstant(offset)
-        case "push":
-            return push(segment,
-                        at: offset,
-                        in: line.fileName)
-        case "pop":
-            return pop(to: segment,
-                       at: offset,
-                       in: line.fileName)
+        switch segment {
+        case "constant":
+            builder.pushConstant(offset)
+            
+        case "temp":
+            accessTemp(command, offset: offset)
+            
+        case "static":
+            accessStatic(command, offset: offset, fileName: line.fileName)
+            
+        case "pointer" where offset == "0":
+            accessPointerZero(command)
+            
+        case "pointer" where offset == "1":
+            accessPointerOne(command)
+            
+        case _ where mnemonic(segment) != nil:
+            accessMnemonic(command, segment: segment, offset: offset)
+            
         default:
             fatalError("Unrecognised memory command '\(command)'")
         }
     }
     
-    func addLabel(_ label: String) -> String {
-        """
-        (\(label))
-        """
+    private func accessStatic(_ c: String, offset: String, fileName: String) {
+        c == "push"
+        ? builder.pushSegmentWithValue("\(fileName).\(offset)", at: "0")
+        : builder.popSegmentWithValue(to: "\(fileName).\(offset)", at: "0")
     }
     
-    func goto(_ label: String) -> String {
-        """
-        @\(label)
-        0;JMP
-        """
+    private func accessPointerZero(_ c: String) {
+        accessMnemonic(c, segment: "this", offset: "0")
     }
     
-    func ifGoto(_ label: String) -> String {
-        """
-        \(popStack())
-        @\(label)
-        D;JEQ
-        """
+    private func accessPointerOne(_ c: String) {
+        accessMnemonic(c, segment: "that", offset: "0")
     }
     
-    func pop(
-        to segment: String,
-        at offset: String,
-        in file: String
-    ) -> String {
-        """
-        \(set("D", to: segment, at: offset, in: file))
-        @R15
-        M=D
-        \(popStack())
-        @R15
-        A=M
-        M=D
-        """
+    private func accessTemp(_ c: String, offset: String) {
+        c == "push"
+        ? builder.pushSegmentWithValue("5", at: offset)
+        : builder.popSegmentWithValue(to: "5", at: offset)
     }
     
-    func push(
-        _ segment: String,
-        at offset: String,
-        in file: String
-    ) -> String {
-        """
-        \(set("A", to: segment, at: offset, in: file))
-        D=M
-        \(pushDToStack())
-        """
+    private func accessMnemonic(_ c: String, segment: String, offset: String) {
+        c == "push"
+        ? builder.pushSegmentWithMnemonic(mnemonic(segment)!,
+                                          at: offset)
+        : builder.popSegmentWithMnemonic(to: mnemonic(segment)!,
+                                         at: offset)
     }
     
-    func set(
-        _ register: String,
-        to segment: String,
-        at offset: String,
-        in file: String
-    ) -> String {
-        switch segment {
-        case _ where mnemonic(segment) != nil:
-            return setRegister(register,
-                               mnemonic: mnemonic(segment)!,
-                               offset: offset)
-        case "pointer" where offset == "0":
-            return setRegister(register,
-                               mnemonic: mnemonic("this")!)
-        case "pointer" where offset == "1":
-            return setRegister(register,
-                               mnemonic: mnemonic("that")!)
-        case "temp":
-            return setRegister(register,
-                               value: "5",
-                               offset: offset)
-        case "static":
-            return setRegister(register,
-                               value: "\(file).\(offset)")
-        default:
-            fatalError("Unrecognised segment '\(segment)'")
-        }
-    }
-    
-    func mnemonic(_ segment: String) -> String? {
+    private func mnemonic(_ segment: String) -> String? {
         switch segment {
         case "local": return "LCL"
         case "argument": return "ARG"
@@ -168,172 +132,5 @@ class VMTranslator {
         case "that": return "THAT"
         default: return nil
         }
-    }
-    
-    func setRegister(
-        _ register: String,
-        mnemonic: String,
-        offset: String = "0"
-    ) -> String {
-        setWithOffsetAddend(register, mnemonic, offset, "M")
-    }
-    
-    func setRegister(
-        _ register: String,
-        value: String,
-        offset: String = "0"
-    ) -> String {
-        setWithOffsetAddend(register, value, offset, "A")
-    }
-    
-    func setWithOffsetAddend(
-        _ register: String,
-        _ value: String,
-        _ offset: String,
-        _ addend: String
-    ) -> String {
-        """
-        @\(offset)
-        D=A
-        @\(value)
-        \(register)=D+\(addend)
-        """
-    }
-    
-    func pushConstant(_ c: String) -> String {
-        c[0] == "-"
-        ? pushNegativeConstant(c)
-        : pushPositiveConstant(c)
-    }
-    
-    func pushPositiveConstant(_ c: String) -> String {
-        """
-        @\(c)
-        D=A
-        \(pushDToStack())
-        """
-    }
-    
-    func pushNegativeConstant(_ c: String) -> String {
-        """
-        @\(c.dropFirst())
-        D=-A
-        \(pushDToStack())
-        """
-    }
-    
-    func pushDToStack() -> String {
-        """
-        \(aEqualsSP())
-        M=D
-        \(incrementSP())
-        """
-    }
-    
-    func eq(_ count: String) -> String {
-        bool("EQ" + count)
-    }
-    
-    func gt(_ count: String) -> String {
-        bool("GT" + count)
-    }
-    
-    func lt(_ count: String) -> String {
-        bool("LT" + count)
-    }
-    
-    func bool(_ predicate: String) -> String {
-        """
-        \(sub())
-        @\(predicate + "_TRUE")
-        D;J\(predicate.prefix(2))
-        D=-1
-        \(replaceTop())
-        @\(predicate + "_FALSE")
-        0;JMP
-        (\(predicate + "_TRUE"))
-        D=0
-        \(replaceTop())
-        (\(predicate + "_FALSE"))
-        """
-    }
-    
-    func add() -> String {
-        binaryOperation("+")
-    }
-    
-    func sub() -> String {
-        binaryOperation("-")
-    }
-    
-    func and() -> String {
-        binaryOperation("&")
-    }
-    
-    func or() -> String {
-        binaryOperation("|")
-    }
-    
-    func binaryOperation(_ o: String) -> String {
-        """
-        \(popStack())
-        A=M-1
-        D=\(o == "-" ? "M-D" : "D\(o)M")
-        \(replaceTop())
-        """
-    }
-    
-    func not() -> String {
-        unaryOperation("!")
-    }
-    
-    func neg() -> String {
-        unaryOperation("-")
-    }
-    
-    func unaryOperation(_ sign: String) -> String {
-        """
-        \(aEqualsSP(offset: "-1"))
-        D=\(sign)M
-        M=D
-        \(replaceTop())
-        """
-    }
-    
-    func aEqualsSP(offset: String = "") -> String {
-        """
-        @SP
-        A=M\(offset)
-        """
-    }
-    
-    func popStack() -> String {
-        """
-        \(aEqualsSP(offset: "-1"))
-        D=M
-        \(decrementSP())
-        """
-    }
-    
-    func replaceTop() -> String {
-        """
-        \(aEqualsSP(offset: "-1"))
-        M=D
-        """
-    }
-    
-    func incrementSP() -> String {
-        stepSP(sign: "+")
-    }
-    
-    func decrementSP() -> String {
-        stepSP(sign: "-")
-    }
-    
-    func stepSP(sign: String) -> String {
-        """
-        @SP
-        M=M\(sign)1
-        """
     }
 }
